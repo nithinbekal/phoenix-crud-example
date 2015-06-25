c996588534c603b8f9412d2ae378e718e0ac9912
Nithin Bekal <nithinbekal@gmail.com>
2015-06-25 22:51:00 +0530

    Refactor current_user to avoid DB when not logged in

--- web/models/session.ex
+++ web/models/session.ex
@@ -13,8 +13,8 @@ defmodule ElixirBlog.Session do
   end
 
   def current_user(conn) do
-    id = Plug.Conn.get_session(conn, :current_user) || 0
-    Repo.get(User, id)
+    id = Plug.Conn.get_session(conn, :current_user)
+    if id, do: Repo.get(User, id)
   end
 
   def logged_in?(conn), do: !!current_user(conn)
